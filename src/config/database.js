e2350d5b449c46702488d6289433223159f6727a
Đặng Nam <nam.dang@pareto.asia>
2024-03-31 08:17:48 +0700

    Add mongoose and change connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,18 @@
-import mysql from "mysql2/promise";
-const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: "root",
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+import mongoose from "mongoose";
+const dbState = [
+  { value: 0, label: "disconnected" },
+  { value: 1, label: "connected" },
+  { value: 2, label: "connecting" },
+  { value: 3, label: "disconnecting" },
+];
+
+const connection = async () => {
+  try {
+    await mongoose.connect("mongodb://root:123456@127.0.0.1:27018");
+    const state = Number(mongoose.connection.readyState);
+    console.log(dbState.find((f) => f.value == state).label, "to db"); // connected to db
+  } catch (error) {
+    console.error(">>>error connect", error);
+  }
+};
 export default connection;
--- src/server.js
+++ src/server.js
@@ -5,15 +5,19 @@ import configViewEngine from "./config/templateEngine.js";
 const app = express();
 const port = process.env.PORT || 8080;
 import webRouter from "./routes/web.js";
+import connection from "./config/database.js";
 
 // configure view engine
-configViewEngine(express, app);
+// configViewEngine(express, app);
 // parsing data form
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
+// connect to server
+connection();
+//
 app.use(webRouter());
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
