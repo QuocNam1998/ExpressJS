cfc0c40cc4abe83d77278dcaf579917907f481d5
Đặng Nam <nam.dang@pareto.asia>
2024-03-31 10:01:33 +0700

    Add simple schema and save table to db

--- src/config/database.js
+++ src/config/database.js
@@ -6,11 +6,31 @@ const dbState = [
   { value: 3, label: "disconnecting" },
 ];
 
+const kittySchema = new mongoose.Schema({
+  name: String,
+});
+// add method into schema
+kittySchema.methods.speak = function speak() {
+  const isHadName = this.name;
+  console.log(`>>>the cat is saying ${isHadName ? "mew mew" : "nothing"}`);
+};
+
+const Kitten = mongoose.model("Kitten", kittySchema);
+const silence = new Kitten({ name: "Nam por" });
+console.log(">>>name is:", silence.name);
+
 const connection = async () => {
   try {
-    await mongoose.connect("mongodb://root:123456@127.0.0.1:27018");
+    const options = {
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASS,
+      dbName: process.env.DB_NAME,
+    };
+    await mongoose.connect(process.env.DB_HOST, options);
     const state = Number(mongoose.connection.readyState);
     console.log(dbState.find((f) => f.value == state).label, "to db"); // connected to db
+    await silence.save();
+    console.log("Document saved successfully!");
   } catch (error) {
     console.error(">>>error connect", error);
   }
